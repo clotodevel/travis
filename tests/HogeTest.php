acd877282a65ed0dbdfb5c49a3732bba899c4786
clo <clo.to.devel@gmail.com>
2016-11-16 10:27:16 +0900

    asdfasdfas

--- tests/HogeTest.php
+++ tests/HogeTest.php
@@ -13,6 +13,7 @@ class HogeTest extends PHPUnit_Framework_TestCase
 		$this->stack = array('1111', '2222', '33333', '43');
 		$this->assertEquals(3, 3);
 		echo $hoge->レディス();
+		echo 'takano';
 		$this->assertEquals('bar', $hoge->レディス());
 
 
